de32db1db871923e43cc8b23e0911490a690ba13
Olga Zotova <olgazotova1976@yandex.ru>
2025-11-17 16:41:18 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -0,0 +1,43 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+class CardDeliveryTest {
+
+    private String generateDate(int daysToAdd, String pattern) {
+        LocalDate today = LocalDate.of(2025, 11, 17); // Set the current date to 17.11.2025
+        LocalDate futureDate = today.plusDays(daysToAdd);
+        return futureDate.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldTestSuccessOrder() {
+        String planningDate = generateDate(3, "dd.MM.yyyy");
+
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79270000000");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.text("Забронировать")).click();
+        $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Успешно! Встреча успешно забронирована на " + planningDate));
+
+    }
+}
